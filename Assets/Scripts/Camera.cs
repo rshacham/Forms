5d6ea7944e15d02ff06a5571788f233b66afa17f
rshacham <roi.shacham@mail.huji.ac.il>
2022-12-15 10:55:12 +0200

    Re-arrange level

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
+    [SerializeField] private float bottomFollowPlayerHeight;
+    [SerializeField] private float upperFollowPlayerHeight;
+
+    public bool FollowPlayerVertical { get; set; } = false;
 
     private Transform _cameraTransform;
     // Start is called before the first frame update
     void Start()
     {
         _cameraTransform = GetComponent<Transform>();
+        FollowPlayerVertical = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _cameraTransform.position = new Vector3(
+        var cameraPosition = _cameraTransform.position;
+        var playerPosition = playerTransform.position;
+        if ((playerPosition.y > bottomFollowPlayerHeight && playerPosition.y < upperFollowPlayerHeight)
+            || FollowPlayerVertical)
+        {
+            cameraPosition.y = playerPosition.y;
+        }
+        if (playerTransform) 
+            _cameraTransform.position = new Vector3(
             playerTransform.position.x,
-            _cameraTransform.position.y,
-            _cameraTransform.position.z);
+            cameraPosition.y,
+            cameraPosition.z);
     }
 }
